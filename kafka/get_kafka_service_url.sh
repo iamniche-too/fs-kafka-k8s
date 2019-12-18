eaa5c2c97960d19e9676032c6f7bc5f6720d904a
iamniche-too <nicholas.oliver.hemley@gmail.com>
2019-12-18 20:19:16 +0000

    Fixed zookeeper claims

--- kafka/get_kafka_service_url.sh
+++
@@ -1,1 +0,0 @@
-minikube service kafka-service --url
--- zookeeper/deploy_zookeeper.sh
+++ zookeeper/deploy_zookeeper.sh
@@ -1,2 +1,4 @@
 kubectl create -f zookeeper/zoo0-service.yaml
+kubectl create -f zookeeper/zoo0-claim0-persistentvolumeclaim.yaml
+kubectl create -f zookeeper/zoo0-claim1-persistentvolumeclaim.yaml
 kubectl create -f zookeeper/zoo0-deployment.yaml
