e53ad6b6e878edfbdb0848356698982fb9827ef0
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-01-21 16:00:04 +0000

    Removed logging/metrics IAM (for now).

--- provision/gcp/iam.tf
+++ provision/gcp/iam.tf
@@ -13,22 +13,22 @@ resource "google_service_account" "default" {
 }
 
 # https://www.terraform.io/docs/providers/google/r/google_project_iam.html
-resource "google_project_iam_member" "logging-log-writer" {
-  role   = "roles/logging.logWriter"
-  member = "serviceAccount:${google_service_account.default.email}"
-}
+#resource "google_project_iam_member" "logging-log-writer" {
+#  role   = "roles/logging.logWriter"
+#  member = "serviceAccount:${google_service_account.default.email}"
+#}
 
-resource "google_project_iam_member" "monitoring-metric-writer" {
-  role   = "roles/monitoring.metricWriter"
-  member = "serviceAccount:${google_service_account.default.email}"
-}
+#resource "google_project_iam_member" "monitoring-metric-writer" {
+#  role   = "roles/monitoring.metricWriter"
+#  member = "serviceAccount:${google_service_account.default.email}"
+#}
 
-resource "google_project_iam_member" "monitoring-viewer" {
-  role   = "roles/monitoring.viewer"
-  member = "serviceAccount:${google_service_account.default.email}"
-}
+#resource "google_project_iam_member" "monitoring-viewer" {
+#  role   = "roles/monitoring.viewer"
+#  member = "serviceAccount:${google_service_account.default.email}"
+#}
 
-resource "google_project_iam_member" "storage-object-viewer" {
-  role   = "roles/storage.objectViewer"
-  member = "serviceAccount:${google_service_account.default.email}"
-}
+#resource "google_project_iam_member" "storage-object-viewer" {
+#  role   = "roles/storage.objectViewer"
+#  member = "serviceAccount:${google_service_account.default.email}"
+#}
