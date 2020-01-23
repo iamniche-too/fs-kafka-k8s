430a3b138bfdd6518d825e2da96df89682f287b6
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-01-23 21:59:18 +0000

    Added script for get pods.

--- deploy/gcp/deploy.sh
+++ deploy/gcp/deploy.sh
@@ -1,5 +1,7 @@
 export GOOGLE_APPLICATION_CREDENTIALS=./deploy/gcp/terraform-test-262517-b4ff2f611b21.json
-export KUBECONFIG=./deploy/gcp/terraform-test-262517-b4ff2f611b21.yaml
+./deploy/gcp/generate-cluster-connection-yaml.sh
 
 kustomize build gcp |\
-kubectl apply -f - 
+kubectl apply -f - --kubeconfig ./deploy/gcp/kubeconfig.yaml
+
+kubectl -n kafka create secret generic dockerhub-secret --from-file=/home/nicholas/.docker/config.json --kubeconfig ./deploy/gcp/kubeconfig.yaml
--- deploy/gcp/undeploy.sh
+++ deploy/gcp/undeploy.sh
@@ -1,6 +1,6 @@
 export GOOGLE_APPLICATION_CREDENTIALS=./deploy/gcp/terraform-test-262517-b4ff2f611b21.json
-export KUBECONFIG=./deploy/gcp/terraform-test-262517-b4ff2f611b21.yaml
+./deploy/gcp/generate-cluster-connection-yaml.sh
 
-kubectl delete clusterrole node-reader
-kubectl delete clusterrolebinding kafka-node-reader
-kubectl delete ns kafka 
+kubectl delete clusterrole node-reader --kubeconfig ./deploy/gcp/kubeconfig.yaml
+kubectl delete clusterrolebinding kafka-node-reader --kubeconfig ./deploy/gcp/kubeconfig.yaml
+kubectl delete ns kafka --kubeconfig ./deploy/gcp/kubeconfig.yaml
