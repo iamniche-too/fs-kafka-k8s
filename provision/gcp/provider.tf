ea026926244565075e1c5fba26d41a4944bb010f
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-01-30 10:43:48 +0000

    Removed Terraform config.

--- provision/gcp/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-  credentials = file("terraform-test-262517-e35fa404a379.json")
-  project = "terraform-test-262517"
-  region  = "europe-west2"
-  zone    = "europe-west2-a"
-}
