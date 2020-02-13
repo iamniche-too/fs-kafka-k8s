76ca28284774c984c7670ce8783027e1f2525752
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-02-13 16:35:27 +0000

    Added Prometheus scraping targets.

--- deploy/gcp/deploy.sh
+++ deploy/gcp/deploy.sh
@@ -4,4 +4,4 @@ export GOOGLE_APPLICATION_CREDENTIALS=./deploy/gcp/terraform-test-262517-b4ff2f6
 kustomize build gcp |\
 kubectl apply -f - --kubeconfig ./deploy/gcp/kubeconfig.yaml
 
-kubectl -n kafka create secret generic dockerhub-secret --from-file=/home/nicholas/.docker/config.json --kubeconfig ./deploy/gcp/kubeconfig.yaml
+#kubectl -n kafka create secret generic dockerhub-secret --from-file=/home/nicholas/.docker/config.json --kubeconfig ./deploy/gcp/kubeconfig.yaml
