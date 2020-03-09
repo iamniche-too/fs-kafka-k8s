cb06a2c719dce7be395e0f4a8bfff12f0bb5718a
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-03-09 16:48:48 +0000

    Using stern for kafka logs.

--- get-kafka-logs.sh
+++ get-kafka-logs.sh
@@ -2,4 +2,4 @@
 source ./export-gcp-credentials.sh
 ./generate-cluster-connection-yaml.sh
 
-kubectl -n kafka logs kafka-$1 --kubeconfig ./kubeconfig.yaml
+stern --namespace=kafka kafka.* --kubeconfig kubeconfig.yaml
