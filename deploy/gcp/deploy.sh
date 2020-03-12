20456a4224748a086de0ab49c9909b134ee9fa95
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-03-12 16:19:12 +0000

    Generate kafka statefulset 3/3.

--- deploy/gcp/deploy.sh
+++ deploy/gcp/deploy.sh
@@ -2,5 +2,8 @@
 source ./export-gcp-credentials.sh
 ./generate-cluster-connection-yaml.sh
 
+# generate the stateful set with the specified number of partitions
+./kafka-statefulset-generate.sh $1 
+
 kustomize build gcp |\
 kubectl apply -f - --kubeconfig ./kubeconfig.yaml
