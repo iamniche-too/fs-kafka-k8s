383cdb102db77e8d64342a709607f110080bbd4b
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-08-10 21:34:37 +0100

    Removed local-provisioner again.

--- undeploy.sh
+++ undeploy.sh
@@ -2,7 +2,8 @@
 source ./export-gcp-credentials.sh
 ./generate-cluster-connection-yaml.sh
 
-kubectl delete pvc local-storage --kubeconfig ./kubeconfig.yaml
+#kubectl delete pvc local-storage --kubeconfig ./kubeconfig.yaml
+#kubectl delete ds local-provisioner --kubeconfig ./kubeconfig.yaml
 
 kubectl delete clusterrole node-reader --kubeconfig ./kubeconfig.yaml
 kubectl delete clusterrolebinding kafka-node-reader --kubeconfig ./kubeconfig.yaml
