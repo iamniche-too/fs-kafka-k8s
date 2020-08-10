074f88f3570a9250b933fd1bbf8b97c6f24ff305
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-08-10 16:52:22 +0100

    Added local-storage to deploy. Removed kustomisation files.

--- undeploy.sh
+++ undeploy.sh
@@ -2,6 +2,8 @@
 source ./export-gcp-credentials.sh
 ./generate-cluster-connection-yaml.sh
 
+kubectl delete pvc local-storage --kubeconfig ./kubeconfig.yaml
+
 kubectl delete clusterrole node-reader --kubeconfig ./kubeconfig.yaml
 kubectl delete clusterrolebinding kafka-node-reader --kubeconfig ./kubeconfig.yaml
 
